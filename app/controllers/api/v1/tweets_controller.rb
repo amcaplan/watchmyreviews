1f0ef3f7612204bd52a0eb8d06a3ce2657b9aaee
amcaplan <ariel.caplan@mail.yu.edu>
2014-05-08 14:12:57 -0400

    Switch to dotenv and make twitter gem work

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -3,4 +3,12 @@ class API::V1::TweetsController < API::V1::APIController
     tweets = client.search('@' + @user.twitter_handle)
     render json: tweets
   end
+
+  private
+    def client
+      Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_API_KEY"]
+        config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+      end
+    end
 end
